e77bc2a8c3fbafe571fd2ce11286567137638bb7
Braxton Bowman <10923641@my.uvu.edu>
2022-04-17 17:52:06 -0600

    Added basic player controls

---
+++ TankGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public float xRange = 20;
+    public float zRange = 8;
+    private Rigidbody playerRb;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves and turns the player based on arrow key input and fires projectile when Space is pressed
+    void MovePlayer()
+    {
+        //Player moves forward/backwards when the up/down arrow keys are pressed
+        //Player turns when the left/right arrow keys are pressed
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+
+        //Player fires projectile when space bar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    
+    }
+
+    //Prevents player from leaving the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+    }
+
+}
